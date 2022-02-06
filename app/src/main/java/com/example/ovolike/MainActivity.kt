12ef9009b003b440cc6b2d5fd7db2fd8f9f16e66
kiohio707 <fernata707@gmail.com>
2022-02-06 20:57:54 +0700

    Second Commit - Payment Tab

--- app/src/main/java/com/example/ovolike/MainActivity.kt
+++ app/src/main/java/com/example/ovolike/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.ovolike
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.ovolike.databinding.ActivityMainBinding
 
 class MainActivity : HelperActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.tvOvoCashMain.text = "500.000"
+
+        binding.toolbarMain.inflateMenu(R.menu.menu_notification_main)
+
+        val menu = binding.toolbarMain.menu
+        menu.findItem(R.id.menu_notification).setOnMenuItemClickListener {
+            Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        binding.ivTopupMain.setOnClickListener {
+            Toast.makeText(this, "Top Up", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.ivTransferMain.setOnClickListener {
+            Toast.makeText(this, "Transfer", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.ivHistoryMain.setOnClickListener {
+            Toast.makeText(this, "Riwayat", Toast.LENGTH_SHORT).show()
+        }
 
 
     }
